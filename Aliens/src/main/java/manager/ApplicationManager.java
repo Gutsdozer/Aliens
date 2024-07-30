8949e6c70d915bb2ae8b6689e38df397a82a9de4
Gutsdozer <warderbass@yandex.ru>
2024-07-30 18:04:34 +0300

    Logger added

--- Aliens/src/main/java/manager/ApplicationManager.java
+++ Aliens/src/main/java/manager/ApplicationManager.java
@@ -2,17 +2,22 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import java.util.concurrent.TimeUnit;
 
+
 public class ApplicationManager {
 
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
     WebDriver wd;
     HelperUser helperUser;
 
     public void init(){
         WebDriver wd = new ChromeDriver();
         wd.navigate().to("https://brutalassault.cz/cs");
+        logger.info("navigated to link ---> "+ wd.getCurrentUrl());
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         helperUser = new HelperUser(wd);
     }
